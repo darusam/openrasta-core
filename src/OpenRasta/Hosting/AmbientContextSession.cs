3e176713bae52053bab4e31cb12e29049508dd47
darusam@cdmsmith.com <darusam@cdmsmith.com>
2014-10-01 00:09:10 -0400

    added IContextSession for access to session variables

---
+++ src/OpenRasta/Hosting/AmbientContextSession.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenRasta.Web;
+
+namespace OpenRasta.Hosting
+{
+    public class AmbientContextSession : IContextSession
+    {
+        public object this[string key]
+        {
+            get
+            {
+                return AmbientContext.Current[key];
+            }
+            set
+            {
+                AmbientContext.Current[key] = value;
+            }
+        }
+
+        public int Timeout
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+                //do nothing
+            }
+        }
+    }
+}
